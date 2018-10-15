0f88f083deb7cbc62fef676e39a30a184bdffe2a
Yifan <tangyf96@outlook.com>
2018-10-14 20:44:10 -0400

    initialize A-star algorithm

---
+++ A-star/A-Star.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 11 19:45:42 2018
+A* grid search algorithms
+@author: Yifan Tang (tangyf96@outlook.com)
+Reference: https://www.redblobgames.com/pathfinding/a-star/implementation.html
+"""
